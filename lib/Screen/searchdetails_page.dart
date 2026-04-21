2ba4f2aee39f6600d0e130ce976a8bbb8f610fce
julitaliita <julitajulita715@gmail.com>
2026-04-21 04:52:17 -0700

    Update searchdetails_page.dart

--- lib/Screen/searchdetails_page.dart
+++ lib/Screen/searchdetails_page.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-import 'food_app_home_screen.dart';
-
 class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
 
@@ -47,7 +45,8 @@ class _SearchScreenState extends State<SearchScreen> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               const SizedBox(height: 16),
-              Text('Search', style: Theme.of(context).textTheme.headlineMedium),
+              Text('Search',
+                  style: Theme.of(context).textTheme.headlineMedium),
               const SizedBox(height: 16),
               const SearchForm(),
               const SizedBox(height: 16),
@@ -56,14 +55,13 @@ class _SearchScreenState extends State<SearchScreen> {
               const SizedBox(height: 16),
               Expanded(
                 child: ListView.builder(
-                  itemCount: _isLoading ? 2 : 5, //5 is demo length of your data
+                  itemCount: _isLoading ? 2 : 5,
                   itemBuilder: (context, index) => Padding(
                     padding: const EdgeInsets.only(bottom: 16),
                     child: _isLoading
                         ? const BigCardScalton()
                         : RestaurantInfoBigCard(
-                            // Images are List<String>
-                            images: demoBigImages..shuffle(),
+                            images: demoBigImages,
                             name: "McDonald's",
                             rating: 4.3,
                             numOfRating: 200,
@@ -105,6 +103,7 @@ class RestaurantInfoBigCard extends StatelessWidget {
     required this.foodType,
     required this.press,
   });
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -112,7 +111,6 @@ class RestaurantInfoBigCard extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // pass list of images here
           BigCardImageSlide(images: images),
           const SizedBox(height: 8),
           Text(name, style: Theme.of(context).textTheme.titleLarge),
@@ -123,44 +121,16 @@ class RestaurantInfoBigCard extends StatelessWidget {
             children: [
               RatingWithCounter(rating: rating, numOfRating: numOfRating),
               const SizedBox(width: 8),
-              SvgPicture.string(
-                clockIconSvg,
-                height: 20,
-                width: 20,
-                colorFilter: ColorFilter.mode(
-                  Theme.of(context)
-                      .textTheme
-                      .bodyLarge!
-                      .color!
-                      .withOpacity(0.5),
-                  BlendMode.srcIn,
-                ),
-              ),
+              SvgPicture.string(clockIconSvg, height: 20, width: 20),
               const SizedBox(width: 8),
-              Text(
-                "$deliveryTime Min",
-                style: Theme.of(context).textTheme.labelSmall,
-              ),
+              Text("$deliveryTime Min"),
               const Padding(
                 padding: EdgeInsets.symmetric(horizontal: 8),
                 child: SmallDot(),
               ),
-              SvgPicture.string(
-                deliveryIconSvg,
-                height: 20,
-                width: 20,
-                colorFilter: ColorFilter.mode(
-                  Theme.of(context)
-                      .textTheme
-                      .bodyLarge!
-                      .color!
-                      .withOpacity(0.5),
-                  BlendMode.srcIn,
-                ),
-              ),
+              SvgPicture.string(deliveryIconSvg, height: 20, width: 20),
               const SizedBox(width: 8),
-              Text(isFreeDelivery ? "Free" : "Paid",
-                  style: Theme.of(context).textTheme.labelSmall),
+              Text(isFreeDelivery ? "Free" : "Paid"),
             ],
           ),
         ],
@@ -170,9 +140,7 @@ class RestaurantInfoBigCard extends StatelessWidget {
 }
 
 class BigCardScalton extends StatelessWidget {
-  const BigCardScalton({
-    super.key,
-  });
+  const BigCardScalton({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -184,9 +152,7 @@ class BigCardScalton extends StatelessWidget {
           child: BigCardImageSlideScalton(),
         ),
         const SizedBox(height: 16),
-        ScaltonLine(
-          width: MediaQuery.of(context).size.width * 0.8,
-        ),
+        ScaltonLine(width: MediaQuery.of(context).size.width * 0.8),
         const SizedBox(height: 16),
         const ScaltonLine(),
         const SizedBox(height: 16),
@@ -197,53 +163,14 @@ class BigCardScalton extends StatelessWidget {
 }
 
 class BigCardImageSlideScalton extends StatelessWidget {
-  const BigCardImageSlideScalton({
-    super.key,
-  });
+  const BigCardImageSlideScalton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Container(
-          decoration: BoxDecoration(
-            color: Colors.black.withOpacity(0.08),
-            borderRadius: const BorderRadius.all(Radius.circular(12)),
-          ),
-        ),
-        Positioned(
-          bottom: 16,
-          right: 16,
-          child: Row(
-            children: List.generate(4, (index) => const DotIndicator()),
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-class DotIndicator extends StatelessWidget {
-  const DotIndicator({
-    super.key,
-    this.isActive = false,
-    this.activeColor = const Color(0xFF22A45D),
-    this.inActiveColor = const Color(0xFF868686),
-  });
-
-  final bool isActive;
-  final Color activeColor, inActiveColor;
-
-  @override
-  Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: const Duration(milliseconds: 250),
-      margin: const EdgeInsets.symmetric(horizontal: 8),
-      height: 5,
-      width: 8,
+    return Container(
       decoration: BoxDecoration(
-        color: isActive ? activeColor : inActiveColor.withOpacity(0.25),
-        borderRadius: const BorderRadius.all(Radius.circular(20)),
+        color: Colors.black.withOpacity(0.08),
+        borderRadius: BorderRadius.circular(12),
       ),
     );
   }
@@ -277,55 +204,83 @@ class SearchForm extends StatefulWidget {
 
 class _SearchFormState extends State<SearchForm> {
   final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Form(
       key: _formKey,
       child: TextFormField(
-        onChanged: (value) {
-          // get data while typing
-          // if (value.length >= 3) showResult();
-        },
-        onFieldSubmitted: (value) {
-          if (_formKey.currentState!.validate()) {
-            // If all data are correct then save data to out variables
-            _formKey.currentState!.save();
-
-            // Once user pree on submit
-          } else {}
-        },
         validator: (value) {
           if (value!.isEmpty) {
             return "Please enter your search query";
           }
           return null;
         },
-        style: Theme.of(context).textTheme.labelLarge,
-        textInputAction: TextInputAction.search,
-        decoration: InputDecoration(
+        decoration: const InputDecoration(
           hintText: "Search on foodly",
-          contentPadding: const EdgeInsets.symmetric(
-            horizontal: 16,
-            vertical: 16,
-          ),
-          prefixIcon: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SvgPicture.string(
-              searchIconSvg,
-              colorFilter: const ColorFilter.mode(
-                Color(0xFF868686),
-                BlendMode.srcIn,
-              ),
-            ),
-          ),
         ),
       ),
     );
   }
 }
 
-const searchIconSvg = '''
-<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fill-rule="evenodd" clip-rule="evenodd" d="M11.1667 17C7.945 17 5.33333 14.3883 5.33333 11.1667C5.33333 7.94502 7.945 5.33334 11.1667 5.33334C14.3883 5.33334 17 7.94502 17 11.1667C17 12.6897 16.4163 14.0764 15.4605 15.1153L18.4226 18.0774C18.748 18.4029 18.748 18.9305 18.4226 19.2559C18.0971 19.5814 17.5695 19.5814 17.2441 19.2559L14.1616 16.1735C13.2862 16.6983 12.2617 17 11.1667 17ZM11.1667 15.3333C13.4678 15.3333 15.3333 13.4679 15.3333 11.1667C15.3333 8.86549 13.4678 7.00001 11.1667 7.00001C8.86547 7.00001 6.99999 8.86549 6.99999 11.1667C6.99999 13.4679 8.86547 15.3333 11.1667 15.3333Z" fill="#010F07"/>
-</svg>
+//// ================= DUMMY =================
+
+const List<String> demoBigImages = [];
+
+class BigCardImageSlide extends StatelessWidget {
+  final List<String> images;
+  const BigCardImageSlide({super.key, required this.images});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 150,
+      color: Colors.grey[300],
+      child: const Center(child: Text("Image Slider")),
+    );
+  }
+}
+
+class PriceRangeAndFoodtype extends StatelessWidget {
+  final List<String> foodType;
+  const PriceRangeAndFoodtype({super.key, required this.foodType});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(foodType.join(", "));
+  }
+}
+
+class RatingWithCounter extends StatelessWidget {
+  final double rating;
+  final int numOfRating;
+
+  const RatingWithCounter({
+    super.key,
+    required this.rating,
+    required this.numOfRating,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("$rating ($numOfRating)");
+  }
+}
+
+class SmallDot extends StatelessWidget {
+  const SmallDot({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("•");
+  }
+}
+
+const String deliveryIconSvg = '''
+<svg viewBox="0 0 24 24"></svg>
+''';
+
+const String clockIconSvg = '''
+<svg viewBox="0 0 24 24"></svg>
 ''';
